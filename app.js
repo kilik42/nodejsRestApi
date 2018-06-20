289eee7a4ffb9ff325892167dc3b503edb30c4bf
marvin evins <maevins@cps.edu>
2018-06-20 15:39:45 -0500

    initial commit

--- app.js
+++ app.js
@@ -1,11 +1,40 @@
 const express = require('express')
 const app = express()
-
 const morgan = require('morgan')
+const mysql = require('mysql')
+
+app.use(morgan('combined'))
 
 
-app.use(morgan('short'))
-//get
+app.get('/user/:id', (req, res)=>{
+  console.log("fetching user with id: "+ req.params.id)
+  const connection = mysql.createConnection({
+    host:'localhost',
+    user: 'root',
+    database: ''
+  })
+
+  const userId = req.params.id
+  const queryString = "SELECT * FROM users WHERE id = ?"
+  connection.query(queryString, [userId], (err, rows, fields)=>{
+    if (err){
+      console.log("failed to queyr fro users: "+ err)
+      res.sendStatus(500)
+
+      return
+    }
+    console.log("i think we fetched users successfully")
+
+    const users = rows.map((row)=> {
+      return {firstName: row.first_name, lastName: row.last_name}
+    })
+    res.json(rows)
+  })
+
+
+  //res.end()
+})
+
 app.get("/", (req, res)=>{
   console.log("responding to root route")
   res.send("hello from root")
@@ -19,15 +48,6 @@ app.get("users", (req, res) =>{
   res.json([user1, user2])
   res.send("nodemon auto updates when i save this file")
 })
-//post
-
-
-
-//put
-
-
-
-//delete
 
 
 
