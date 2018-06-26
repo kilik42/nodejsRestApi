f9ea843b8daf049780c53ca1c7bfdb438b748d9e
marvin evins <maevins@cps.edu>
2018-06-25 21:40:02 -0500

    form creation and made dynamic
    form creation and made dynamic

--- app.js
+++ app.js
@@ -3,16 +3,50 @@ const app = express()
 const morgan = require('morgan')
 const mysql = require('mysql')
 
-app.use(morgan('combined'))
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended: false}))
+
+app.use(express.static('./public'))
+
+app.use(morgan('short'))
+
+app.post('/user_create', (req, res)=> {
+
+  console.log("tyring to create a new user")
+  console.log("how do we get the form data?")
+
+console.log("first name: "+ req.body.create_first_name)
+ const firstName = req.body.create_first_name
+ const lastName = req.body.create_last_name
+
+const queryString = "INSERT INTO users (first_name, last_name) VALUES (?,?)"
+getConnection().query(queryString, [firstName, lastName], (err, results, fields)=>{
+  if (err){
+    console.log("Failed to insert new user: "+ err)
+    res.sendStatus(500)
+    return
+  }
+  console.log("Inserted a new user with id: "+ results.insertedId);
+  res.end()
+})
+  res.end()
+
+})
+
+function getConnection(){
+  return mysql.createConnection({
+    host:'localhost',
+    user: 'root',
+    database: 'users'
+  })
+}
 
 
 app.get('/user/:id', (req, res)=>{
   console.log("fetching user with id: "+ req.params.id)
-  const connection = mysql.createConnection({
-    host:'localhost',
-    user: 'root',
-    database: ''
-  })
+  const connection = getConnection()
+
 
   const userId = req.params.id
   const queryString = "SELECT * FROM users WHERE id = ?"
